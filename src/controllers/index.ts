2c5bdf92f015ab8c3498a8d3637c16b61a19a176
jabjee <90618917+jabjee@users.noreply.github.com>
2021-09-13 20:00:22 +0700

    added simple message

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,19 +1,30 @@
 import { Request, Response } from "express";
 import { middleware } from "@line/bot-sdk";
+import * as line from "@line/bot-sdk";
 /**
  * GET /
  * Home page.
  */
 
+const client  = new line.Client({
+    channelAccessToken:process.env.LINE_CHANNEL_ACCESS_TOKEN,
+});
 export const index = async (req: Request, res: Response): Promise<void> => {
     res.status(404).send();
 };
 
 export const webhook = async (req: Request, res: Response): Promise<void> => {
-    console.log(req.body.events);
-    console.log(req.body.destination);
-    res.status(200).send({
-        status: "ok",
-        message: "Webhook received",
+    const message:any = {
+        "type": "text",
+        "text": "ขอขอบคุณ -คุณลูกค้ามากๆครับที่มาใช้บริการถ่ายภาพของเรา😍📸 \n Facebook: Sakon Damminsek \n เบอร์ติดต่อส่วนตัวช่างภาพ    \n📱(0658273828)",
+    };
+
+    console.log(req.body.events[0].message.text);
+    console.log(req.body.events[0].source.userId);
+    client.pushMessage(req.body.events[0].source.userId, message).then(() => {
+        console.log("Message has been sent");
+    }).catch((err) => {
+        console.error("Message has been sent");
     });
+   
 };
