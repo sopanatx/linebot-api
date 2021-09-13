855d6719b92ff8cabe76b9efc51318297049ca47
jabjee <90618917+jabjee@users.noreply.github.com>
2021-09-13 20:14:10 +0700

    Update index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -20,11 +20,36 @@ export const webhook = async (req: Request, res: Response): Promise<void> => {
     console.log({userMessage});
     console.log({userId});
    
+    const template:any = {
+        "template": {
+            "type": "buttons",
+            "actions": [
+              {
+                "uri": "https://www.facebook.com/Sakondamminsek2401",
+                "label": "ติดต่อช่างภาพโดยตรง",
+                "type": "uri",
+              },
+              {
+                "label": "เบอร์ที่สามารถติดต่อ",
+                "text": "0658273828",
+                "type": "postback",
+                "data": "0658273828",
+              },
+            ],
+            "title": "ติดต่อช่างภาพโดยตรง",
+            "thumbnailImageUrl": "https://sv1.picz.in.th/images/2020/09/20/O5Df7E.md.jpg",
+            "text": "ทางเราจะติดต่อกลับไปให้ไวที่สุ๊ดดดด 👌😁📱",
+          },
+          "type": "template",
+          "altText": "this is a buttons template",
+        
+    };
     switch(userMessage){
         case "ติดต่อช่างภาพโดยตรง":
             const message:any = {
                 "type": "text",
                 "text": "ขอขอบคุณ -คุณลูกค้ามากๆครับที่มาใช้บริการถ่ายภาพของเรา😍📸 \n Facebook: Sakon Damminsek \n เบอร์ติดต่อส่วนตัวช่างภาพ    \n📱(0658273828)",
+                ...template
             };
             client.pushMessage(req.body.events[0].source.userId, message).then(() => {
                 console.log("Message has been sent");
