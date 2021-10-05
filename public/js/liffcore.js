7f58a34f5f5faecddaafade7a20b9463fb5e61c7
Sopanat Suradach <sopanus2014@gmail.com>
2021-10-05 10:06:17 +0700

    Update liffcore.js

--- public/js/liffcore.js
+++ public/js/liffcore.js
@@ -1,9 +1,9 @@
 window.onload = function () {
     const useNodeJS = true // if you are not using a node server, set this value to false
-    const defaultLiffId = '' // change the default LIFF value if you are not using a node server
+    const defaultLiffId = '1656487149-D51gEMmn' // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
-    let myLiffId = ''
+    let myLiffId = '1656487149-D51gEMmn'
 
     // if node is used, fetch the environment variable and pass it to the LIFF method
     // otherwise, pass defaultLiffId
